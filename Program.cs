86b53eb2a420907e75d2f00b4ac5f1e644f4f5cb
Andrey Frolov <50494877+Frolov-Andrey2405@users.noreply.github.com>
2023-02-23 21:59:09 +0200

    Program

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1;
+
+IMenu1_PresentationLayer menu1_PresentationLayer = new Menu1_PresentationLayer();
+IMenu2_PresentationLayer menu2_PresentationLayer = new Menu2_PresentationLayer();
+Menu1 first_Menu = new Menu1(menu1_PresentationLayer);
+Menu2 second_Menu = new Menu2(menu2_PresentationLayer);
+MainMenu mainMenu = new MainMenu(first_Menu, second_Menu);
+
+mainMenu.Run();
