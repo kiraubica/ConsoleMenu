726708aada34724b11c4fc8064d6bada71c5748d
Andrey Frolov <50494877+Frolov-Andrey2405@users.noreply.github.com>
2023-02-23 21:53:32 +0200

    Menu1

---
+++ IMenu1_presentationlayer.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    internal interface IMenu1_PresentationLayer
+    {
+        void Menu1_1();
+        void Menu1_2();
+    }
+}
---
+++ Menu1_presentationlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Menu1_PresentationLayer : IMenu1_PresentationLayer
+    {
+        public void Menu1_1()
+        {
+            Console.WriteLine("Menu 1.1");
+            Console.ReadKey();
+        }
+        public void Menu1_2()
+        {
+            Console.WriteLine("Menu 1.2");
+            Console.ReadKey();
+        }  
+    }
+}
