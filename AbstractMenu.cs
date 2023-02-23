5c026587924d4cf4e9be40acb628fd5a04816697
kolya2555d56 <126194729+kolya2555d56@users.noreply.github.com>
2023-02-23 21:48:01 +0200

    AbstractClass

---
+++ AbstractMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal abstract class AbstractMenu
+    {
+        protected bool flag = true;
+
+        protected abstract void Init();
+        protected abstract void Idle();
+
+        protected abstract void CleanUp();
+
+        public void Run()
+        {
+            Init();
+            while (flag)
+            {
+                Idle();
+            }
+            CleanUp();
+        }
+    }
+}
